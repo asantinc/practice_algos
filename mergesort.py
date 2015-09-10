07d4baa8023fb2d37b64947377e170fda923990a
asantinc <angiedaniela@gmail.com>
2015-09-11 00:30:21 +0200

    Added bubble sort and insertion sort

--- mergesort.py
+++ mergesort.py
@@ -1,7 +1,45 @@
 
-import pdb
+def insertion_sort(array):
+	# print 'Time complexity is O(n^2) (remember sum of integers)'
+	# print 'Space complexity is O(1)'
+	for i, item in enumerate(array):
+		if i == 0:
+			continue
+		j = i-1
+		while j>=0 and item<array[j]:
+			temp = item
+			array[i] = array[j]
+			array[j] = temp
+			j -= 1
+	return array
+
+
+def bubble_sort(array):
+	# print 'Time complexity is O(n^2): at each pass over the array \
+	# 		only the last element is guaranteed to be in the correct \
+	# 		position.'
+	# print 'Space complexity is O(1)'
+	swap = True
+	for end in range(len(array)-2):
+		if swap:
+			swap = False
+			offset = len(array)-end
+			for i, item in enumerate(array[1:offset]):
+
+				if item < array[i]:
+					#swap
+					swap = True
+					temp = array[i]
+					array[i] = array[i+1]
+					array[i+1] = temp
+	return array
+
 
 def merge(array, low, mid, high):
+	# print 'Time complexity is O(n*log(n)): \
+	# 				splitting a list of size n takes log(n) time, \
+	# 				and sorting each sublist takes O(n) time'
+	# print 'Space complexity is O(n)'
 	sorted_array = list()
 
 	i = low
@@ -38,12 +76,31 @@ def mergesort(array, low=None, high=None):
 	return merge(array, low, mid, high)
 
 
-a = [6,5,7,3,4,11,33,9,10]
-b = [-33, 6,7,8,6,-6,19]
-c = [-33, 6,7,8,6,-6]
-d = [0]
+if __name__ == '__main__':
+	a = [6,5,7,3,4,11,33,9,10]
+	b = [-33, 6,9,8,5,-6,19]
+	c = [-33, 6,99,8,6,-6]
+	d = [0]
+	e = []
 
-print mergesort(a)
-print mergesort(b)
-print mergesort(c)
-print mergesort(d)
+	print mergesort(a)
+	print mergesort(b)
+	print mergesort(c)
+	print mergesort(d)
+	print mergesort(e)
+
+	print insertion_sort(a)
+	print insertion_sort(b)
+	print insertion_sort(c)
+	print insertion_sort(d)
+	print mergesort(e)
+
+	print bubble_sort(a)
+	print bubble_sort(b)
+	print bubble_sort(c)
+	print bubble_sort(d)
+	print mergesort(e)
+
+
+
+
