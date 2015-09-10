4fdca306e497bfca0c6d9da7aaac3284cbb66f73
asantinc <angiedaniela@gmail.com>
2015-09-10 22:54:47 +0200

    Added iterative vs. recursive

--- binary_search.py
+++ binary_search.py
@@ -1,20 +1,42 @@
-def binary_search(array, item, low=None, high=None):
+def binary_search_recursive(array, item, low=None, high=None):
 	low = low if low is not None else 0
 	high = high if high is not None else len(array)
 	mid = (low+high)/2
 
-	while low<mid:
+	if low<mid:
 		if array[mid] > item:
-			return binary_search(array, item, low=low, high=mid)
+			return binary_search_recursive(array, item, low=low, high=mid)
 		elif array[mid] < item:
-			return binary_search(array, item, low=mid, high=high)
+			return binary_search_recursive(array, item, low=mid, high=high)
 		else:
 			return 'Item found at index {}'.format(mid)
 	return 'Item not found'
 
 
+def binary_search_iterative(array, item, low=None, high=None):
+	low = 0
+	high = len(array)
+	mid = (low+high)/2
+
+	while low<mid:
+		if array[mid] > item:
+			low=low 
+			high=mid
+		elif array[mid] < item:
+			low=mid
+			high=high
+		else:
+			return 'Item found at index {}'.format(mid)
+		
+		mid = (low+high)/2
+
+	return 'Item not found.'
+
+
 a = [-2, 4, 5, 5,6, 7]
-print binary_search(a, 6)	
+print binary_search_recursive(a, 6)	
+print binary_search_iterative(a, 6)	
 
 b = [-2, 4, 5,6, 7]
-print binary_search(b, 6)	
+print binary_search_recursive(b, 6)	
+print binary_search_iterative(b, 6)	
